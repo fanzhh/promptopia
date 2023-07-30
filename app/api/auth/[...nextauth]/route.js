99ef2a5de3bed53f663d88d25cddc9664a204bc9
fanzhh <fanzhh@gmail.com>
2023-07-30 21:20:01 +0800

    init

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,68 @@
+import NextAuth from "next-auth/next";
+import GitHubProvider from 'next-auth/providers/github';
+
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth({
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        })
+    ],
+    callbacks: {
+        async session({ session }){
+            console.log('in session');
+            try {
+                await connectToDB();
+                const sessionUser = await User.findOne({
+                    email: session.user.email
+                });
+                /* 以下为新增 */
+                if (!sessionUser) {
+                    // Create a new user if one doesn't exist
+                    console.log('will create new user.')
+                    const newUser = await User.create({
+                        email: session.user.email,
+                        username: session.user.name.replace(" ","").toLowerCase(),
+                        image: session.user.picture
+                    });
+                    session.user.id = newUser._id.toString();
+                } else {
+                    session.user.id = sessionUser._id.toString();
+                }
+                /* 结束 */
+                //session.user.id = sessionUser._id.toString();
+            } catch(error) {
+                console.log('error in session callback:',error);
+            }         
+            
+            return session;
+        },
+        async SignIn({ profile }) {
+            console.log('in sign in');
+            try {
+                await connectToDB();
+                // check if a user already exists.
+                const userExists = await User.findOne({
+                    email: profile.email,
+                });
+                // if not create a new user.
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ","").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log("Error in SignIn callback:",error);
+                return false;
+            } 
+        } 
+    },
+});
+
+export { handler as GET, handler as POST };
---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(isConnected) {
+        console.log('MongoDB is connected.');
+        return;
+    }
+
+    console.log('will connect to mongoose...');
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 30000, // 增加超时时间为 30 秒钟
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true;
+        console.log('MongoDB is connected.');
+    } catch (error) {
+        console.log('connect db error:', error);
+    }
+}
